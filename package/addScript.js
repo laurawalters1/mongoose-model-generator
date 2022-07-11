4e6eec56f8162b5e9804b4a71167d734b6431005
Laura Walters <laura.walters1098@yahoo.com>
2022-07-11 18:04:59 +0100

    updating json

--- package/addScript.js
+++ package/addScript.js
@@ -2,21 +2,21 @@ const fs = require("fs");
 
 let parsedData;
 fs.readFile("../../package.json", "utf8", (err, data) => {
-	if (err) {
-		console.log(err);
-		return;
-	}
-	parsedData = JSON.parse(data);
-	console.log("test");
-	parsedData.scripts["create:model"] =
-		"cd node_modules && cd mern-model-generator && npm run create:model";
-	fs.writeFile(
-		"../../package.json",
-		JSON.stringify(parsedData, null, 4),
-		(err) => {
-			if (err) {
-				console.log(err);
-			}
-		}
-	);
+  if (err) {
+    console.log(err);
+    return;
+  }
+  parsedData = JSON.parse(data);
+  console.log("test");
+  parsedData.scripts["create:model"] =
+    "cd node_modules && cd mongoose-model-templates && npm run create:model";
+  fs.writeFile(
+    "../../package.json",
+    JSON.stringify(parsedData, null, 4),
+    (err) => {
+      if (err) {
+        console.log(err);
+      }
+    }
+  );
 });
