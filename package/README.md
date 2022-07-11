22155fb3736cb0bd77514ac460f9aaef7779e42b
Laura Walters <laura.walters@clpt.co.uk>
2022-07-11 15:39:24 +0100

    file setup

---
+++ package/README.md
@@ -0,0 +1,34 @@
+Run the following command in the server directory to generate a template mern model (replace 'User' with your model name)
+
+```javascript
+npm run create:model -name=User
+```
+
+The file will be located in the models folder, if a models folder does not already exist, one will be created
+<br>
+<br>
+Output:
+
+```javascript
+const { Schema, model } = require("mongoose");
+
+const moment = require("moment");
+
+const userSchema = new Schema(
+	{
+		createdAt: {
+			type: Date,
+			default: Date.now,
+		},
+	},
+	{
+		toJSON: {
+			virtuals: true,
+		},
+	}
+);
+
+const User = model("User", userSchema);
+
+module.exports = User;
+```
---
+++ package/index.js
@@ -0,0 +1,39 @@
+const argv = require("minimist")(process.argv.slice(2));
+const fs = require("fs");
+const { npm_config_name: name } = process.env;
+
+const data = `
+const { Schema, model } = require("mongoose");
+
+const moment = require("moment");
+
+const ${name.toLowerCase()}Schema = new Schema(
+  {
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+
+const ${name} = model("${name}", ${name.toLowerCase()}Schema);
+
+module.exports = ${name};
+`;
+
+if (!fs.existsSync("../../models")) {
+	fs.mkdirSync("../../models");
+}
+
+fs.writeFile(`../../models/${name}.js`, data, (err) => {
+	if (err) {
+		console.error(err);
+	}
+	// file written successfully
+});
