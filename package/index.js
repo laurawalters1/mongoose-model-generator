534abfafa114aee48755682fd34a4027144f5960
Laura Walters <laura.walters1098@yahoo.com>
2022-10-29 14:00:04 +0100

    Fixing model name bug

--- package/index.js
+++ package/index.js
@@ -6,7 +6,12 @@ const { Schema, model } = require("mongoose");
 
 const moment = require("moment");
 
-const ${name.toLowerCase()}Schema = new Schema(
+const ${name
+  .split("")
+  .reduce(
+    (t, c, i) => (i === 0 ? t.concat(c.toLowerCase()) : t.concat(c)),
+    ""
+  )}Schema = new Schema(
   {
     createdAt: {
       type: Date,
@@ -21,44 +26,50 @@ const ${name.toLowerCase()}Schema = new Schema(
 );
 
 
-const ${name} = model("${name}", ${name.toLowerCase()}Schema);
+const ${name} = model("${name}", ${name
+  .split("")
+  .reduce(
+    (t, c, i) => (i === 0 ? t.concat(c.toLowerCase()) : t.concat(c)),
+    ""
+  )}Schema);
 
 module.exports = ${name};
 `;
 
 if (!fs.existsSync("../../models")) {
-	fs.mkdirSync("../../models");
+  fs.mkdirSync("../../models");
 }
 
 if (!fs.existsSync("../../models/index.js") && x) {
-	fs.writeFileSync(
-		"../../models/index.js",
-		`const ${name} = require("./${name}");\n\nmodule.exports = {\n    ${name},\n};`
-	);
+  fs.writeFileSync(
+    "../../models/index.js",
+    `const ${name} = require("./${name}");\n\nmodule.exports = {\n    ${name},\n};`
+  );
 } else if (x) {
-	let testData;
-	fs.readFile("../../models/index.js", "utf8", (err, data) => {
-		testData = data;
-		let models = data.slice(data.indexOf("{") + 1, data.indexOf("}"));
-		let newModels = models.concat(`    ${name},`);
+  let testData;
+  fs.readFile("../../models/index.js", "utf8", (err, data) => {
+    testData = data;
+    let models = data.slice(data.indexOf("{") + 1, data.indexOf("}"));
+    let newModels = models.concat(`    ${name},`);
 
-		let newData = data.replace(/\{(\d|\w|\s|,)+\}/gm, `{${newModels}\n}`);
+    let newData = data.replace(/\{(\d|\w|\s|,)+\}/gm, `{${newModels}\n}`);
 
-		fs.writeFileSync(
-			"../../models/index.js",
-			`const ${name} = require("./${name}");\n${newData}`,
-			(err) => {
-				if (err) {
-					console.log(err);
-				}
-			}
-		);
-	});
+    fs.writeFileSync(
+      "../../models/index.js",
+      `const ${name} = require("./${name}");\n${newData}`,
+      (err) => {
+        if (err) {
+          console.log(err);
+        }
+      }
+    );
+  });
 }
 
 fs.writeFile(`../../models/${name}.js`, data, (err) => {
-	if (err) {
-		console.error(err);
-	}
-	// file written successfully
+  if (err) {
+    console.error(err);
+  }
+  console.log(`${name} model has successfully been created!`);
+  // file written successfully
 });
